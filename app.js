8dcdc83a0e68cae79e181c4c1ad101fb9f19bf5b
Raul GM <loscanos3raul@gmail.com>
2023-10-04 19:26:26 +0200

    Final trabajo

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ function Zapa() {
     var cami=document.getElementById('cami')
     var cintu=document.getElementById('cintu')
     zapa.addEventListener('click', function(){
-        zapatilla.style.backgroundColor='black'
-        cami.style.backgroundColor='black'
-        cintu.style.backgroundColor='black'
+        zapatilla.style.backgroundColor='white'
+        zapatilla.style.color='black'
+        cami.style.backgroundColor='rgb(31, 28, 28)'
+        cintu.style.backgroundColor='rgb(31, 28, 28)'
+        cami.style.color='white'
+        cintu.style.color='white'
     })
 }
 function Cami() {
@@ -15,9 +18,12 @@ function Cami() {
     var camiseta=document.getElementById('cami')
     var cintu=document.getElementById('cintu')
     cami.addEventListener('click', function(){
-        camiseta.style.backgroundColor='black'
-        zapa.style.backgroundColor='black'
-        cintu.style.backgroundColor='black'
+        camiseta.style.backgroundColor='white'
+        camiseta.style.color='black'
+        zapa.style.backgroundColor='rgb(31, 28, 28)'
+        cintu.style.backgroundColor='rgb(31, 28, 28)'
+        zapa.style.color='white'
+        cintu.style.color='white'
     })
 }
 function Cintu() {
@@ -26,8 +32,11 @@ function Cintu() {
     var cami=document.getElementById('cami')
     var cinturon=document.getElementById('cintu')
     cintu.addEventListener('click', function(){
-        cinturon.style.backgroundColor='black'
-        cami.style.backgroundColor='black'
-        zapa.style.backgroundColor='black'
+        cinturon.style.backgroundColor='white'
+        cinturon.style.color='black'
+        cami.style.backgroundColor='rgb(31, 28, 28)'
+        zapa.style.backgroundColor='rgb(31, 28, 28)'
+        cami.style.color='white'
+        zapa.style.color='white'
     })
 }
